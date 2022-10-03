92829c55c90f2a305a4d75580b2b901a14eb7f07
Janes Rodrigues <engaero.jns@gmail.com>
2022-10-03 07:03:47 -0300

    Atualização de tabelas

--- Modelos/ScriptSQLMySql.sql
+++ Modelos/ScriptSQLMySql.sql
@@ -2,25 +2,14 @@ create database alphaRural;
 
 use alphaRural;
 
-CREATE TABLE Apoiador  (
-email VARCHAR(40),
+CREATE TABLE Apoiador(
 dataNasc DATETIME,
 nome VARCHAR(50),
 telefone VARCHAR(11),
 tipoDoacao text,
 telContato VARCHAR(11),
+email VARCHAR(30),
+senha VARCHAR(10),
 id_Apoiador INTEGER PRIMARY KEY
 );
 
-CREATE TABLE Acesso (
-id_acesso INTEGER PRIMARY KEY,
-email VARCHAR(30),
-senha VARCHAR(6)
-);
-
-CREATE TABLE Relacao1 (
-id_acesso INTEGER,
-id_Apoiador INTEGER,
-FOREIGN KEY(id_acesso) REFERENCES Acesso (id_acesso),
-FOREIGN KEY(id_Apoiador) REFERENCES Apoiador  (id_Apoiador)
-);
