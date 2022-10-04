fe72d05900a286e3f0e9d3631d8ccf7fb173d2b7
Michelle Rodrigues da S. Sales <michellesalesrodrigues23@gmail.com>
2022-10-03 23:20:32 -0300

    criação do projeto java

---
+++ projetoSquad39/src/main/java/projetoSquad39/ApoiadorDAO.java
@@ -0,0 +1,18 @@
+package projetoSquad39;
+
+import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ApoiadorDAO {
+	
+	/*** CRUD creat, read, update d delete ***/
+	
+	
+	
+
+}
